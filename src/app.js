ef9fc4024e3e2e0d899bcb0642d0d58450d802a4
Alvaro Ponce Marin <147168257+superteclas@users.noreply.github.com>
2024-02-22 00:58:26 +0000

    funciona todo menos undefined

--- src/app.js
+++ src/app.js
@@ -6,6 +6,45 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Declaro las variables//
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let palos = [1, 2, 3, 4];
+  let iconos = ["♠️", "♥️", "♦️", "♣️"];
+
+  //función para generar una carta aleatoria
+  function generarCarta() {
+    //Hago los arrais aleatorios//
+    let randomPaloIndex = Math.floor(Math.random() * palos.length);
+    let randomNumeroIndex = Math.floor(Math.random() * numbers.length);
+
+    //Indico los indices//
+    let randomPalo = palos[randomPaloIndex]; //pregunta para mentoria, no se porque me funciona el -1
+    let randomNumero = numbers[randomNumeroIndex];
+    let iconoPalo = iconos[randomPalo];
+
+    //Enlazo con el HTML//
+    document.querySelector(".paloarriba").innerHTML = iconoPalo;
+    document.querySelector(".numerocentro").innerHTML = randomNumero;
+    document.querySelector(".paloabajo").innerHTML = iconoPalo;
+  }
+
+  // Hago la primera carta cuando se carga la página
+  generarCarta();
+
+  // Se carga una carta aleatoria cada 10 segundos (está en milisegundos)
+  setInterval(generarCarta, 10000);
 };
